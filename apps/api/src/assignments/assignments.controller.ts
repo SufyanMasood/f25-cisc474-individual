3484a86394cfca3ae54cf0ff26a46073fab714a2
Sufyan Masood <sufyanm@udel.edu>
2025-10-21 22:46:11 -0400

    Install zod, uuid, create DTOs, and update assignement's service and controller

--- apps/api/src/assignments/assignments.controller.ts
+++ apps/api/src/assignments/assignments.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete } from '@nestjs/common';
 import { AssignmentsService } from './assignments.service';
+import { AssignmentCreateIn, AssignmentUpdateIn } from '@repo/api/assignments';
 
 @Controller('assignments')
 export class AssignmentsController {
@@ -13,5 +14,20 @@ export class AssignmentsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.assignmentsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createDto: AssignmentCreateIn) {
+    return this.assignmentsService.create(createDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateDto: AssignmentUpdateIn) {
+    return this.assignmentsService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.assignmentsService.remove(id);
   }
 }
